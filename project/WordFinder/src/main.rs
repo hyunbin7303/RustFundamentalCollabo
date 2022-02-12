5a014884d598cd496c89f0d1b7385c70100d8709
kevin <hyunbin7303@gmail.com>
2022-02-12 15:34:46 -0500

    Update argument configuration update

--- project/WordFinder/src/main.rs
+++ project/WordFinder/src/main.rs
@@ -24,10 +24,25 @@ use std::env::args;
 // itself and its captures.
 
 
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 
 struct Config {
     query: String,
     filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str>{
+        if args.len() <3 {
+            return Err("Not enough arguments. Please check the requirement.");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config {query, filename})
+    }
 }
 fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
@@ -44,8 +59,10 @@ fn find_inputword(input: &str) -> &str {
     ""
 }
 
-fn generate_txtfile() -> Result<(), Error> {
-    let mut file = File::create("test.txt")?;
+fn generate_txtfile(filename: &str) -> Result<(), Error> {
+    let mut s = filename.to_string();
+    s.pop();
+    let mut file = File::create(&s)?;
     file.write_all(b"Hello, World! \nhow are you? my name is Kevin.")?;
     Ok(())
 }
@@ -57,7 +74,6 @@ fn trim_newline(s: &mut String){
         }
     }
 }
-
 fn read_textfile(filename: &str) -> Result<(), Error> {
     //trim_newline(&filename);
     let mut s = filename.to_string();
@@ -82,23 +98,47 @@ fn read_textfile(filename: &str) -> Result<(), Error> {
 fn insert_str_front(s: &mut String, input_str: String){
     s.insert_str(0, &input_str,)
 }
+fn check_file_exist(filename: &str) -> bool {
+    let mut s = filename.to_string();
+    s.pop();
+
+    let file = std::path::Path::new(&s).exists();
+    file
+}
 
 
 fn main() -> std::io::Result<()> {
 
-    let input = args();
-    input.skip(1).for_each(|item| {
-        println!("You wrote {}, which in capital letters in {}", item, item.to_uppercase());
-    });
+    // let input = args();
+    // input.skip(1).for_each(|item| {
+    //     println!("You wrote {}, which in capital letters in {}", item, item.to_uppercase());
+    // });
 
+    let pattern = args().nth(1).expect("No Pattern given");
+    // let args_Cli = Cli {
+    //     pattern: pattern,
+    //     path: std::path::PathBuf::from(path),
+    // };
+    let args_vec : Vec<String> = args().collect();
+    let config = parse_config(&args_vec);
+    println!("Searching for {}", config.query);
+    println!("In file : {}", config.filename);
     // let opt: Option<String> = Some("Some value".to_owned());
     // let value = opt.as_deref().unwrap_or("default string");
 
     let mut txt_name = String::new();
     println!("Type your text file name.");
     let bl = std::io::stdin().read_line(&mut txt_name).unwrap();
-  //  generate_txtfile();
-    read_textfile(&txt_name); // put test.txt file in here for testing.
+
+    // Checking if file exists in the directory.
+    let is_exist = check_file_exist(&txt_name);
+    if is_exist {
+        println!("File Exists!");
+        read_textfile(&txt_name); // put test.txt file in here for testing.
+    }else{
+        println!("File doesn't exist. Create file : {}", txt_name);
+        generate_txtfile(&txt_name); 
+    }
     
     // use insert_str_front 
     // And store again
