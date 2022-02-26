2803cb72ada4ddc0f75c62beeb2ae8010c1112d6
kevin <hyunbin7303@gmail.com>
2022-02-26 16:09:14 -0500

    Config yaml update practice

--- project/cli_clap2/src/main.rs
+++ project/cli_clap2/src/main.rs
@@ -5,7 +5,7 @@ extern crate clap;
 extern crate slog;
 
 use std::process;
-use clap::{Arg, App, ArgMatches, Command, Parser };
+use clap::{Arg, App, load_yaml, ArgMatches, Command, Parser };
 use std::ffi::OsString;
 
 #[derive(Parser)]
@@ -25,19 +25,25 @@ struct Cli {
 // }
 
 fn main() {
+
+    let yaml = loal_yaml!("config.yaml");
+
+
     let matches = Command::new("KevinApp")
         .version("1.0")
         .author("Kevin Park. <hyunbin7303@gmail.com>")
         .about("Does awesome things")
-        .arg(Command::new("CONFIG")
+        .arg(Arg::new("CONFIG")
             .short('c')
             .long("config")
             .help("Sets a custom config file")
             .takes_value(true))
         .arg(Arg::new("INPUT")
+            .short('i')
+            .long("INPUT")
             .help("Sets the input file to use")
-            .required(true)
-            .index(1))
+            .takes_value(true))
+            // .index(1))
         .subcommand(Command::new("test")
             .about("controls testing features")
             .version("1.3")
@@ -56,19 +62,21 @@ fn main() {
                 .help("Kevin study clap!")
                 .required(false)
                 .value_name("KEVIN PARK")))
+
+        .subcommand(Command::new("run")
+            .about("Runs the Foo server")
+            .arg(Arg::new("debug")
+                .short('D')
+                .help("Sends debug foos instead of normal foos.")))
         .subcommands( vec![
             Command::new("config").about("Controls configuration functionality").arg(Arg::with_name("config_file").index(1)),
             Command::new("debug").about("Controls debug functionality")
         ])
-        
-        .get_matches();
+    .get_matches();
 
 
     println!("Using input file: {}", matches.value_of("INPUT").unwrap());
-
-    let config = matches.value_of("CONFIG").unwrap_or("default.conf");
-    println!("Value of config: {}", config);
-
+    let config_file = matches.value_of("CONFIG").unwrap_or("config.json");
     if let Some(matches) = matches.subcommand_matches("test") {
         if matches.is_present("verbose") {
             println!("Printing verbosely...");
@@ -83,5 +91,6 @@ fn main() {
             println!("Not printing check...");
         }
     }
+
 
 }
---
+++ project/yaml_rust/config.rs
@@ -0,0 +1,12 @@
+
+
+
+// pub fn get_yaml_config(config_file: &str) -> Array {
+//     let path_to_file = Path::new(&config_file);
+//     let display = path_to_file.display();
+//     let mut fd = match File::(&path_to_file){
+//         Err(why) => panic!("Couldn't open this file {}: {}", display, why.description()),
+//         Ok(file) => file
+//     };
+//     `````````
+// }
